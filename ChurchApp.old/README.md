5d79ea757e7ed6dd4c246cf28d04f4b807df1e2a
rRui69 <114761253+rRui69@users.noreply.github.com>
2026-02-03 11:04:48 +0800

    Delete ChurchApp.old directory

--- ChurchApp.old/README.md
+++
@@ -1,6 +0,0 @@
-# App Dev - 2025
-<h1>Church App</h1>
-<p>This Part serves as the documentation for the repository of this project.</p>
-
-Documentation:
-
--- ChurchApp.old/test_connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-require "my_auth/db.php";
-
-if ($conn) {
-    echo "MySQL connection successful!";
-} else {
-    echo "MySQL connection failed: " . mysqli_connect_error();
-}
-?>
